f4749a912d3ec120e1c830f04bf97ce79f7889f7
klopez851 <keidy081lopez@gmail.com>
2026-07-20 06:58:06 -0400

    added first few test classes, as well as removed unneeded dependencies

---
+++ src/test/java/com/example/notesAPI/Unit Tests/service/NoteColorServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.notesAPI.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoteColorServiceTest {
+
+    @Test
+    void createNoteColor() {
+    }
+
+    @Test
+    void getNoteColors() {
+    }
+
+    @Test
+    void updateNoteColor() {
+    }
+
+    @Test
+    void deleteCoteColor() {
+    }
+}
